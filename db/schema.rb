7a721086a94e9f24e6dab5deb32f3188bae373a5
bafiam <stevebafiam@gmail.com>
2020-02-27 01:50:20 +0300

    add comment validation and relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_220958) do
+ActiveRecord::Schema.define(version: 2020_02_26_223412) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
